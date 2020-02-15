d8793ba0e8e7aba68f33c739facc945b1d5cf018
Arief Gusti <arief.gusti@gmail.com>
2020-02-16 01:54:27 +0700

    test destructuring assignments

--- app/index.js
+++ app/index.js
@@ -1,5 +1,16 @@
 //=========================================================================
-//test spread operator
+//test destructuring assignments
+let mobil = ['avanza', 'teana', 'xpander'];
+let [toyota, nissan, mitsubishi, renault] = mobil;
+console.log(toyota, nissan);
+console.log(toyota, nissan, mitsubishi, renault); //alphard will be undefined
+
+let sekolah = { namasekolah: 'YPPI', tingkatan: 'SD', murid: 2000 };
+let {namasekolah, tingkatan} = sekolah;
+console.log(namasekolah, tingkatan);
+
+//=========================================================================
+//test spread operator and rest parameters
 let arrayA = [30, 50, 70];
 let arrayB = [10, ...arrayA, 90];
 console.log(arrayB);
