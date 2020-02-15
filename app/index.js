8d0752a66d5928ec10fc2575887f8c557e4ff081
Arief Gusti <arief.gusti@gmail.com>
2020-02-16 01:24:16 +0700

    Test let and const

--- app/index.js
+++ app/index.js
@@ -1,2 +1,20 @@
+const name = "Raffi";
+
+function hello(name) {
+    let message = "Halooo...";
+    console.log(message, name);
+}
+
+function greeting() {
+    let message = "Apa kabar";
+    //name = "Dodi"; <= will throw error
+    console.log(message, name);
+}
+
+hello(name);
+greeting();
+
+//=========================================================================
+//test webpack
 console.log('hello webpack');
 console.log('show in browser');
